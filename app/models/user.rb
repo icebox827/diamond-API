bc83c8075383e68ff1d73a8acbd67364c1d670a9
Denis <icebox827@gmail.com>
2022-03-18 19:40:00 -0400

    Fix bug in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ApplicationRecord
   # validations
   has_secure_password 
 
-  validates :first_name, presence: true, length: { in 2..25 }
-  validates :last_name, presence: true, length: { in 2..25 }
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 25 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :email, presence: true, uniqueness: { case_sensitive: true }
   validates_format_of :email, with: /\A\S+@.+\.\S+\z/
-  validates :password, presence: true, length { in 8..30 }
+  validates :password, presence: true, length: { minimum: 8, maximum: 25 }
 
   # associations
 end
