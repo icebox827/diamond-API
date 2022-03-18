069758602418940d02515f531c09483a0018c26e
Denis <icebox827@gmail.com>
2022-03-18 15:10:14 -0400

    Create validatioons for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  # validations
+  has_secure_password 
+
+  validates :first_name, presence: true, length: { in 2..25 }
+  validates :last_name, presence: true, length: { in 2..25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: true }
+  validates_format_of :email, with: /\A\S+@.+\.\S+\z/
+  validates :password, presence: true, length { in 8..30 }
+
+  # associations
 end
