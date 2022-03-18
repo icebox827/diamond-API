462000e7d502751627810bec6369770a4569d289
Denis <icebox827@gmail.com>
2022-03-18 19:26:49 -0400

    Update user controller

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,7 +1,44 @@
 class Api 
   module V1
     class UserController < ApplicationController
-      
+      # Display all users in json format
+      def index
+        @users = User.all
+        render json: @users
+      end
+
+      # Display a single user in json format
+      def show
+        @user = User.find(params[:id])
+        render json: @user
+      end
+
+      # Create a user and render json format
+      def create
+        @user = User.new(user_params)
+
+        if @user.save
+          render json: { message: 'User created successfully' }, status: 200
+        else
+          render json: { error: @user.error_full_message }, status: 422
+        end
+      end
+
+      # Update a single user and render json format
+      def update
+        @user = User.find(params[:id])
+
+        if @user.update
+          render json: { message: 'User created successfully' }, status: 200
+        else
+          render json: { error: @user.error_full_message }, status: 422
+      end
+
+      private
+      # User parameters
+      def user_params
+        params.permit(:first_name, :last_name, :email, :password)
+      end
     end
   end
 end
