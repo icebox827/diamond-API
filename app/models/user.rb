8ec9a8941c36de047f29aaffcefd7959520d5879
Denis <icebox827@gmail.com>
2022-03-19 11:35:17 -0400

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :last_name, presence: true, length: { minimum: 2, maximum: 30 }
   validates :email, presence: true, uniqueness: { case_sensitive: true }
   validates_format_of :email, with: /\A\S+@.+\.\S+\z/
-  validates :password, presence: true, length: { minimum: 8, maximum: 25 }
+  validates :password, presence: true, length: { minimum: 8 }
 
   # associations
 end
