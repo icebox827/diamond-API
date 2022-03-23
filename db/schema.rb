ac532cbb32d1bee23021630be238b74348b805ad
Denis <icebox827@gmail.com>
2022-03-22 21:48:52 -0400

    Add user reference to services

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_23_014249) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_23_014721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_23_014249) do
     t.integer "car_quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_services_on_user_id"
   end
 
   create_table "transmissions", force: :cascade do |t|
@@ -75,4 +77,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_23_014249) do
   add_foreign_key "cars", "transmissions"
   add_foreign_key "cars", "users"
   add_foreign_key "phones", "users"
+  add_foreign_key "services", "users"
 end
