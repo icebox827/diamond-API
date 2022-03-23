12d76a640c78ab0db85ebfbdfca7a30e9c0157f8
Denis <icebox827@gmail.com>
2022-03-22 21:21:27 -0400

    Create service model

---
+++ db/migrate/20220323012114_create_services.rb
@@ -0,0 +1,10 @@
+class CreateServices < ActiveRecord::Migration[7.0]
+  def change
+    create_table :services do |t|
+      t.string :service
+      t.integer :car_quantity
+
+      t.timestamps
+    end
+  end
+end
