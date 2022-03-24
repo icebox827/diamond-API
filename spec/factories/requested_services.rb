f047e6b61f6e232eb2478d905099902fea5b1d6b
Denis <icebox827@gmail.com>
2022-03-23 22:42:23 -0400

    Create foreign key table

---
+++ spec/factories/requested_services.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :requested_service do
+    user_id { nil }
+    service_id { nil }
+  end
+end
---
+++ spec/models/requested_service_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe RequestedService, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
