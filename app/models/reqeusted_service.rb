96a25235db73fc7c55295a714effe87cac83391d
Denis <icebox827@gmail.com>
2022-03-23 23:16:49 -0400

    Create assocaition and validations for req

--- app/models/reqeusted_service.rb
+++ app/models/reqeusted_service.rb
@@ -1,2 +1,9 @@
 class ReqeustedService < ApplicationRecord
+  # validations
+  validates :user_id, presence: true
+  validates :service_id, presence: true
+  
+  # Associations
+  belongs_to :User
+  belongs_to :service
 end
