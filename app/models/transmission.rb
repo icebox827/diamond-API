58aca47c1fa3447caf956501387f448dfed5ab57
Denis <icebox827@gmail.com>
2022-03-23 23:08:15 -0400

    Add association and validations to cars

--- app/models/transmission.rb
+++ app/models/transmission.rb
@@ -1,2 +1,8 @@
 class Transmission < ApplicationRecord
+  # validations
+  validates :transmission_type, presence: true
+  validates :abbreviation, presence: true
+
+  # associations
+  has_many :cars
 end
