cb2df9d1c89aa12ad388e5981dbf97eef3597e4a
Denis <icebox827@gmail.com>
2022-03-25 16:53:30 -0400

    Update services controllers

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -1,2 +1,38 @@
-class ServicesController < ApplicationController
+module Api
+  module V1
+    class ServicesController < ApplicationController
+      # Display all services
+      def index
+        @services = Service.all
+        render json: @services
+      end
+
+      # Create services
+      def create
+        @service = Service.new(service_params)
+
+        if @service.save
+          render json: { messages: 'Service created successfully' }, status: 200
+        else
+          render json: { error: @service.errors.full_messages }, status: 422
+        end
+      end
+
+      # Update service
+      def update
+        @service = Service.find(params[:id])
+
+        if @service.update
+          render json: { message: 'Service update successfully' }, status: 200
+        else
+          render json: { error: @service.errors.full_messages }, status: 422
+        end
+      end
+
+      # Service parameters
+      def service_params
+        params.permit(:service)
+      end
+    end
+  end
 end
