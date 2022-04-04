db6c2101c885da84a819c0effeb689d1bb39b244
Denis <icebox827@gmail.com>
2022-04-04 18:04:55 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1, default: { format: :json } do
       resources :users, only: %i[index show create update] do
-        resource :address, only: %i[index show create update]
+        resource :profile, only: %i[index show create update destroy]
+        resources :cars, only: %i[index show create update destroy]
       end
       resources :authentication, only: %i[create]
       resources :services, only: %i[index create update]
       resources :reqeusted_services, only: %i[index show create update destroy]
-      resources :phones, only: %i[index show create update destroy]
-      resources :cars, only: %i[index show create update destroy]
     end
   end
 end
